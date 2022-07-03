d412e7000bc35a20512301034645b6780b40e810
Luiza Pereira Uszacki <luizapereirauszacki@gmail.com>
2022-07-03 00:12:10 -0300

    Adição do hover effect e adição do Dialog

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,60 @@
 const $ = (x) => document.querySelector(x)
 
-$("#submit-btn").addEventListener('click', () => {
-    let text = $("#first-text").value
+
+//Input Texts
+const fistInputText =$("#first-text")
+const secondInputText =$("#second-text")
+//Error Container
+const errorContainer = $('.error-container')
+//Buttons
+const copyButton = $("#copy-btn")
+const resetButton = $('#reset-btn')
+const okButton = $('#ok-btn')
+
+
+
+//EventListeners
+fistInputText.addEventListener('input', changeTextInput)
+copyButton.addEventListener('click', copyText)
+window.addEventListener('load', () => window.scrollTo(0, 0))
+resetButton.addEventListener('click', startTyping)
+
+
+
+
+//Functions
+function changeTextInput() {
+    let text = fistInputText.value
+    secondInputText.value = changeTexttoRot13(text)
+}
+
+
+function copyText() {
+    let text = secondInputText.value
     try {
-        if (!text) throw new Error('Empty value')
-        if (!text.trim()) throw new Error('Empty value')
-        $("#second-text").value = rot13(text)
+        if (!text) throw new Error('Valor vazio.')
+        if (!text.trim()) throw new Error('Valor vazio.')
+        navigator.clipboard.writeText(text)
     }
     catch(e) {
-        console.log(e)
+        $('#error-text').textContent = (e.message === 'Valor vazio.') ? e.message + ' Adicione texto ao campo e tente novamente.' : e.message
+        errorContainer.showModal()
+
+        okButton.addEventListener('click', () => {
+            errorContainer.close()
+            startTyping()
+        })
     }
-})
+}
 
-console.log(document.querySelector('main').offsetHeight)
 
-function rot13(message){
+function startTyping() {
+    window.scrollTo(0, 0)
+    fistInputText.focus()
+}
+
+
+function changeTexttoRot13(message){
     const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     const letters = message.split('')
 
